e9bc7f72b8bdf6928880f3daf822ba8f235d7380
Роман Авраменко <roma.avramencko2012@yandex.ru>
2022-11-07 07:57:08 +0300

    Third attempt

--- homework.py
+++ homework.py
@@ -47,9 +47,7 @@ def send_message(bot, message):
 
 
 def get_api_answer(current_timestamp):
-    """
-    Получение данных с АПИ Яндекс Практикума.
-    """
+    """Получение данных с АПИ Яндекс Практикума."""
     timestamp = current_timestamp or int(time.time())
     params = {'from_date': timestamp}
 
